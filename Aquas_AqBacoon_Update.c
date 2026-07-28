6117d2e0e7dca0fd7dbfd60a1c639c7e76a24ea0
akratch <kratch.adam@gmail.com>
2026-07-28 23:11:00 +0200

    Coordinate Aquas first-geometry float webs
    Combining the case-18 four-web loop with X=var_fs0, Y=sp104, Z=spF8 removes all three spill instructions. Calibrated result: word1017/raw1042/norm731/reg318, exact 3032 instructions and -296 frame. decomp.me cyJAV improves from prior gold10495 to 9480.

--- Aquas_AqBacoon_Update.c
+++ Aquas_AqBacoon_Update.c
@@ -585,11 +585,11 @@ void Aquas_AqBacoon_Update(AqBacoon* this) {
         this->swork[AQ_SWK_18] &= 3;
     }
     if ((this->state >= 4) && (this->state < 16)) {
-        spF4 = gPlayer[0].pos.x - this->obj.pos.x;
-        var_fs0 = gPlayer[0].pos.y - this->obj.pos.y;
-        spE0 = gPlayer[0].trueZpos - this->obj.pos.z;
-        spE8 = Math_RadToDeg(Math_Atan2F(spF4, spE0));
-        spE4 = Math_RadToDeg(-Math_Atan2F(var_fs0, sqrtf((spF4 * spF4) + (spE0 * spE0))));
+        var_fs0 = gPlayer[0].pos.x - this->obj.pos.x;
+        sp104 = gPlayer[0].pos.y - this->obj.pos.y;
+        spF8 = gPlayer[0].trueZpos - this->obj.pos.z;
+        spE8 = Math_RadToDeg(Math_Atan2F(var_fs0, spF8));
+        spE4 = Math_RadToDeg(-Math_Atan2F(sp104, sqrtf((var_fs0 * var_fs0) + (spF8 * spF8))));
         if ((spE8 <= 40.0f) || (spE8 >= 320.0f)) {
             Math_SmoothStepToAngle(&D_i3_801C4308[15], spE8, 1.0f, 5.0f, 0.001f);
         }
