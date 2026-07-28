e64ac4b4d18053c30304c609f1a958913868451f
akratch <kratch.adam@gmail.com>
2026-07-29 00:06:17 +0200

    Order Aquas geometry loads for target schedule

--- Aquas_AqBacoon_Update.c
+++ Aquas_AqBacoon_Update.c
@@ -628,11 +628,11 @@ void Aquas_AqBacoon_Update(AqBacoon* this) {
     }
 
     var_fs0 = gPlayer[0].pos.x - D_i3_801C4308[73];
-    spF4 = gPlayer[0].pos.y - D_i3_801C4308[77];
     spF8 = gPlayer[0].pos.x - D_i3_801C4308[76];
+    temp3 = gPlayer[0].pos.y - D_i3_801C4308[74];
+    spF4 = gPlayer[0].pos.y - D_i3_801C4308[77];
     spFC = gPlayer[0].trueZpos - D_i3_801C4308[75];
     spF0 = gPlayer[0].trueZpos - D_i3_801C4308[78];
-    temp3 = gPlayer[0].pos.y - D_i3_801C4308[74];
     spE8 = Math_RadToDeg(Math_Atan2F(var_fs0, spFC));
     spE0 = Math_RadToDeg(Math_Atan2F(spF8, spF0));
     spFC = sqrtf((var_fs0 * var_fs0) + (spFC * spFC));
